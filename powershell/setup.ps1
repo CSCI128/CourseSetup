2a8be2cdb123e924c94c5c3b280016d4790c89b8
Gregory Bell <gjbell@trihardstudios.com>
2024-08-19 16:08:31 -0600

    Remove extra env reload code

--- powershell/setup.ps1
+++ powershell/setup.ps1
@@ -67,17 +67,6 @@ if (-Not $?) {
 # Optional: For bash.exe, add 'C:\Program Files\Git\bin' to PATH
     [Environment]::SetEnvironmentVariable('Path', "$([Environment]::GetEnvironmentVariable('Path', 'Machine'));C:\Program Files\Git\bin", 'Machine')
 
-# Make new environment variables available in the current PowerShell session:
-    foreach($level in "Machine","User") {
-       [Environment]::GetEnvironmentVariables($level).GetEnumerator() | % {
-          # For Path variables, append the new values, if they're not already in there
-          if($_.Name -match 'Path$') { 
-             $_.Value = ($((Get-Content "Env:$($_.Name)") + ";$($_.Value)") -split ';' | Select -unique) -join ';'
-          }
-          $_
-       } | Set-Content -Path { "Env:$($_.Name)" }
-    }
-
 }
 
 refresh-path
