8a052209730fcbcc210ee583374d95700b175ebc
Gregory Bell <trihard.studios@outlook.com>
2025-01-29 09:09:46 -0700

    Update setup.sh

--- linux-apt/setup.sh
+++ linux-apt/setup.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt-get install python3.12-full
+sudo apt-get install -y python3.12-full
 wget https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py --break-system-packages
 echo alias python=python3 >> ~/.bashrc
@@ -8,15 +8,14 @@ source ~/.bashrc
 pip install 128Autograder --break-system-packages
 pip install matplotlib --break-system-packages
  
-sudo apt-get install wget gpg
+sudo apt-get install -y wget gpg
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
 rm -f packages.microsoft.gpg
  
-sudo apt install apt-transport-https
-sudo apt update
-sudo apt-get install code
+sudo apt install -y apt-transport-https
+sudo apt-get install -y code
  
 code --install-extension ms-python.python
  
